6b16aacd6f0cd87aa16ea510c51d18852cb7c03f
wuyawei <wuyawei@enn.cn>
2022-10-26 21:01:33 +0800

    feat: add eslint

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,24 @@
 <script setup lang="ts">
-const websiteName = 'doecjt'
+import { Console } from 'console'
 
+const websiteName = 'doecjt'
+const a = 'test'
+const b = false
+
+function foo() {
+  if (b)
+    return
+  debugger
+  if (a)
+    console.log('a', a)
+
+  Console.log('hello')
+}
 </script>
 
 <template>
   <div>
     <div>{{ websiteName }}</div>
-    <div></div>
+    <div />
   </div>
 </template>
